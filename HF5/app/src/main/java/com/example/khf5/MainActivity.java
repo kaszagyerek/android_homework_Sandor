14d2a9bccb981fb94f0d9fae090eb09a1cf0e605
Sándor Kasza <kasza.sandor@yahoo.com>
2021-12-15 09:53:14 +0200

    HF5

--- HF5/app/src/main/java/com/example/khf5/MainActivity.java
+++ HF5/app/src/main/java/com/example/khf5/MainActivity.java
@@ -1,45 +1,109 @@
 package com.example.khf5;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.viewpager.widget.ViewPager;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.khf5.ui.main.SectionsPagerAdapter;
-import com.example.khf5.databinding.ActivityMainBinding;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> nevek = new ArrayList<>();
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        nevek.add("Sandor");
+        nevek.add("Geza");
+        nevek.add("Imre");
+        nevek.add("Janos");
+        nevek.add("Molnar");
+        nevek.add("Erika");
+        nevek.add("Anna");
+        nevek.add("Juliska");
 
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = binding.viewPager;
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = binding.tabs;
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = binding.fab;
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        list = findViewById(R.id.list);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, nevek);
+        registerForContextMenu(list);
+        list.setAdapter(adapter);
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option, menu);
+        return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getMenuInflater().inflate(R.menu.contex, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.sort )
+        {
+            Collections.sort(nevek, String.CASE_INSENSITIVE_ORDER);
+            adapter.notifyDataSetChanged();
+            Toast.makeText(MainActivity.this, "kilistázva", Toast.LENGTH_SHORT).show();
+            return true;
+        } else  if (item.getItemId() == R.id.delete )
+        {
+            nevek.clear();
+            adapter.notifyDataSetChanged();
+            Toast.makeText(MainActivity.this, "törölve", Toast.LENGTH_SHORT).show();
+            return true;
+        } else  {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        View v = info.targetView;
+        TextView view = (TextView) v.findViewById(android.R.id.text1);
+
+        if (item.getItemId() == R.id.red )
+        {
+            view.setTextColor(Color.RED);
+            Toast.makeText(MainActivity.this, "változik pirosra", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else if (item.getItemId() == R.id.green )
+        {
+            view.setTextColor(Color.GREEN);
+            Toast.makeText(MainActivity.this, "változik zöldre", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == R.id.yellow )
+        {
+            view.setTextColor(Color.YELLOW);
+            Toast.makeText(MainActivity.this, "változik sárgára", Toast.LENGTH_SHORT).show();
+            return true;
+        } else {
+            return super.onContextItemSelected(item);
+        }
+
+
+        }
+    }
